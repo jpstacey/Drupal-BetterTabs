8910c9e60f00434fe1bf1363c8468a7141736efc
J-P Stacey <me@jpstacey.info>
2010-01-31 17:19:12 +0000

    First checkin of codebase

---
+++ templates/bettertabs-section.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file BetterTabs section
+ */
+?>
+<div class="bettertabs-section bettertabs-section-<?php print $class ?>">
+  <h3><?php print $title ?></h3>
+  <?php print theme('links', $links) ?>
+</div>
---
+++ templates/bettertabs-sections.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file BetterTabs sections
+ */
+?>
+<?php if ($content): ?>
+<div id="bettertabs">
+  <?php foreach($content as $section) { print $section; } ?>
+</div>
+<?php endif; ?>
